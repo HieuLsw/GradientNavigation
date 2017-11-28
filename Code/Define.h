02b3943a7c7b9b733d65408e15c313cb83b5ea25
王家伟 <542413041@qq.com>
2017-11-28 16:24:54 +0800

    Update Define.h

--- Code/Define.h
+++ Code/Define.h
@@ -1,1 +1,14 @@
+#ifndef AppDefine_h
+#define AppDefine_h
 
+// 是否是iPhone X
+#define kIPHONEX ((kScreenW == 375.0f) && (kScreenH == 812.0f)) ? YES : NO
+#define kDevice_Is_iPhoneX ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) : NO)
+// iPhone X底部的返回按钮高度
+#define kIPHONEXBOTTOM ((kIPHONEX) ? 34.0 : 0.0)
+// iPhone X导航栏高度
+#define kIPHONEXNAVHEIGHT ((kIPHONEX) ? 88.0f : 64.0f)
+// iPhone X状态栏高度
+#define kIPHONEXSTAUSHEIGHT ((kIPHONEX) ? 44.0f : 20.0f)
+
+#endif
