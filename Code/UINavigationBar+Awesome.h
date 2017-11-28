762a1469fc4fe8df3a1396aa764806a1ea02c9ed
王家伟 <542413041@qq.com>
2017-11-28 16:23:44 +0800

    Add files via upload

---
+++ Code/UINavigationBar+Awesome.h
@@ -0,0 +1,16 @@
+//
+//  UINavigationBar+Awesome.h
+//  ZFTQPOS
+//
+//  Created by swift on 2017/8/4.
+//  Copyright © 2017年 hkrt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationBar (Awesome)
+- (void)lt_setBackgroundColor:(UIColor *)backgroundColor;
+- (void)lt_setElementsAlpha:(CGFloat)alpha;
+- (void)lt_setTranslationY:(CGFloat)translationY;
+- (void)lt_reset;
+@end
